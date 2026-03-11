34e8df0baf214dc37a0318efe6e28be1f385fd65
Daniil <astarothos202@gmail.com>
2026-03-11 13:56:43 +0200

    Update config.py

--- car_bot/config.py
+++ car_bot/config.py
@@ -1,8 +1,9 @@
 # Токен бота (получите у @BotFather)
-BOT_TOKEN = "8659263004:AAGOxRjp2QZPexKmlFWlmXpa5opbcFPAamY"  # ВСТАВЬТЕ СЮДА
+BOT_TOKEN = "Bot token"  # ВСТАВЬТЕ СЮДА
 
 # Файл с данными
 DATA_FILE = "data/cars.json"
 
 # Версия
+
 VERSION = "1.0.0"
